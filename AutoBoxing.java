6fd73449e28537658718529949d7c1d10dbcef65
Sharifullah Amin <103334899+SharifullahCS@users.noreply.github.com>
2022-11-25 00:07:14 -0800

    Collection Freamwork Best Concepts

---
+++ AutoBoxing.java
@@ -0,0 +1,10 @@
+package Boxing;
+
+public class AutoBoxing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Generics.java
@@ -0,0 +1,10 @@
+package Generics;
+
+public class Generics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
